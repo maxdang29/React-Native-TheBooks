5963c857ece5ab15975fe4957ea66bc35c63fa09
Nam-Dang-Dev <nam.dang@student.passerellesnumeriques.org>
2020-01-06 14:58:18 +0700

    setup project

---
+++ android/app/src/main/java/com/reactnativeproject/MainApplication.java
@@ -0,0 +1,41 @@
+package com.reactnativeproject;
+
+
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+
+import java.util.List;
+
+import java.util.Arrays;
+import com.reactnativenavigation.NavigationApplication;
+import com.reactnativenavigation.react.NavigationReactNativeHost;
+import com.reactnativenavigation.react.ReactGateway;
+
+public class MainApplication extends NavigationApplication {
+
+    @Override
+    protected ReactGateway createReactGateway() {
+        ReactNativeHost host = new NavigationReactNativeHost(this, isDebug(), createAdditionalReactPackages()) {
+            @Override
+            protected String getJSMainModuleName() {
+                return "index";
+            }
+        };
+        return new ReactGateway(this, isDebug(), host);
+    }
+
+    @Override
+    public boolean isDebug() {
+        return BuildConfig.DEBUG;
+    }
+
+    protected List<ReactPackage> getPackages() {
+        return Arrays.<ReactPackage>asList();
+    }
+
+    @Override
+    public List<ReactPackage> createAdditionalReactPackages() {
+        return getPackages();
+    }
+
+}
---
+++ index.js
@@ -0,0 +1,19 @@
+/**
+ * @format
+ */
+
+import {Navigation} from 'react-native-navigation';
+
+import App from './src/app';
+
+Navigation.registerComponent(`navigation.playground.WelcomeScreen`, () => App);
+
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      component: {
+        name: 'navigation.playground.WelcomeScreen',
+      },
+    },
+  });
+});
