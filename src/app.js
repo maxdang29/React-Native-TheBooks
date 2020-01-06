f51dac479c7c9687581817a094a1dac629183486
Nam-Dang-Dev <nam.dang@student.passerellesnumeriques.org>
2020-01-06 15:50:52 +0700

    update dependencies

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export default class App extends Component {
   render() {
     return (
       <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
         <Text>Hello, world!</Text>
+        <Icon name="ios-trash" solid />
       </View>
     );
   }
